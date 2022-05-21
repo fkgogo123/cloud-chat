88a8792fc880009cd0733c325e4bf547e67c5824
fkgogo123 <45331462+fkgogo123@users.noreply.github.com>
2022-05-21 21:08:52 +0800

    Add files via upload

---
+++ InstantMessaging/src/main/java/com/example/component/UserController.java
@@ -0,0 +1,17 @@
+package com.example.component;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+
+// @Controller
+// public class UserController {
+//
+//     @RequestMapping("/imserver/login")
+//     public void login(RequestBody request) {
+//         this.
+//     }
+// }
