2ae1b4e0b34ef972209b59698309ebc37eccda23
fkgogo123 <45331462+fkgogo123@users.noreply.github.com>
2022-05-21 21:13:18 +0800

    Add files via upload

---
+++ im-vue-2/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div>
+
+  <el-input v-model="user.username" placeholder="请输入登录用户名"></el-input>
+  
+  <el-row>
+    <el-button type="primary" @click="submit">登录</el-button>
+  </el-row>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  // name: "addUser",
+  data() {
+    return {
+      user: {
+        username: 'zhangsan'
+      }
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.user.username);
+      sessionStorage.setItem("user", JSON.stringify(this.user));
+      console.log("登录,存session数据:" + JSON.parse(sessionStorage.getItem("user")).username);
+      this.$router.push("/im")
+      
+    },
+  }
+
+}
+
+</script>
